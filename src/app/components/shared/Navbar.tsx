b144164abeccc866061f6db4777bf1463a7991d7
Naim Hasan <naimislam24ff@gmail.com>
2026-09-21 17:32:05 +0600

    read books update

--- src/app/components/shared/Navbar.tsx
+++ src/app/components/shared/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
               <Link href="/listed-books">Listed Books</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link href="/read-books">Book Vibe</Link>
             </li>
           </ul>
         </div>
