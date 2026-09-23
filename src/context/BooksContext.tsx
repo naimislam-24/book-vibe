08d3414cc9965cf8d91b2fcee58bb6160a39700a
Naim Hasan <naimislam24ff@gmail.com>
2026-09-23 18:53:51 +0600

    add read-book card

--- src/context/BooksContext.tsx
+++ src/context/BooksContext.tsx
@@ -1,10 +1,12 @@
 "use client";
+// import { IBook } from "@/app/types/Books.type";
 import { createContext, ReactNode, useState } from "react";
 
 export const BooksContext = createContext({});
 
 const BooksProvider = ({ children }: { children: ReactNode }) => {
   const [readBooks, setReadeBooks] = useState([]);
+  // const [readBooks, setReadeBooks] = useState<IBook[]>([]);
   const [wishlist, setWishlist] = useState([]);
 
   const sharedData = {
